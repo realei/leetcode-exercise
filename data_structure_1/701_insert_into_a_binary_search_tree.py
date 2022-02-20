f39c8e97e68b8ec87ade9801a4d01ccf075d3db1
Lei Wang <wanglei.okay@gmail.com>
2022-02-20 18:51:43 +0200

    #701

---
+++ data_structure_1/701_insert_into_a_binary_search_tree.py
@@ -0,0 +1,32 @@
+"""
+You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+
+Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+
+        node = root
+        while node:
+            if val < node.val:
+                if node.left:
+                    node = node.left
+                else:
+                    node.left = TreeNode(val)
+                    break
+            else:
+                if node.right:
+                    node = node.right
+                else:
+                    node.right = TreeNode(val)
+                    break
+        return root
