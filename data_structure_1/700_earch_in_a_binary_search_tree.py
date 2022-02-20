ef143ee5b23961dece56994930891d54d9665e88
Lei Wang <wanglei.okay@gmail.com>
2022-02-20 18:49:51 +0200

    leetcode: #700

---
+++ data_structure_1/700_earch_in_a_binary_search_tree.py
@@ -0,0 +1,23 @@
+"""
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+
+        while root:
+
+            if root.val == val:
+                return root
+
+            root = root.left if root.val > val else root.right
+
+        return None
