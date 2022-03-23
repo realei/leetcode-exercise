a7d553d416517432a7309e935f1d6ea143d1bc9d
Lei Wang <wanglei.okay@gmail.com>
2022-03-23 22:16:26 +0200

    chore

---
+++ daily_exercise/290_word_pattern.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        ch2word = {}
+        word2ch = {}
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        for ch, word in zip(pattern, words):
+            if (word in word2ch and word2ch[word] != ch) or (ch in ch2word and ch2word[ch] != word):
+                return False
+            word2ch[word] = ch
+            ch2word[ch] = word
+            
+        return True
