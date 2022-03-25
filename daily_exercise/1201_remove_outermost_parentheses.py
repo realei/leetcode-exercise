22778e7f9e381ce5a24ae76172bbbf4b747e6312
Lei Wang <wanglei.okay@gmail.com>
2022-03-25 13:24:07 +0200

    #1201

---
+++ daily_exercise/1201_remove_outermost_parentheses.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeOuterParentheses(self, s):
+        ret = ""
+        stack = []
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                left = stack.pop()
+                if not stack:
+                    ret += s[left + 1:i]
+        return ret
