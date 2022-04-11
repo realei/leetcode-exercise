e67026b6b76c84001a2264b41f6d95204cf66167
Lei Wang <wanglei.okay@gmail.com>
2022-04-12 00:16:44 +0300

    #703

---
+++ data_structure/heap/703_Kth_Largest_Element_in_a_Stream.py
@@ -0,0 +1,27 @@
+class KthLargest:
+    """
+    TBD
+    """
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.min_heap = nums
+        heapq.heapify(self.min_heap)
+
+    def add(self, val: int) -> int:
+        """
+        TBD
+        """
+        heapq.heappush(self.min_heap, val)
+        
+        while len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+        
+        return self.min_heap[0]
+        
+
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
