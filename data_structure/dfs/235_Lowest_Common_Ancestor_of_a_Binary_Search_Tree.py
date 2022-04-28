afa0feb97ed2aa7bf3b8a038192237f72d01c8be
Lei Wang <wanglei.okay@gmail.com>
2022-04-28 14:20:27 +0300

    dfs: #235

---
+++ data_structure/dfs/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        ancestor = root
+        
+        while ancestor:
+            if ancestor.val > p.val and ancestor.val > q.val:
+                ancestor = ancestor.left
+            elif ancestor.val < p.val and ancestor.val < q.val:
+                ancestor = ancestor.right
+            else:
+                return ancestor
+        
+        return ancestor
