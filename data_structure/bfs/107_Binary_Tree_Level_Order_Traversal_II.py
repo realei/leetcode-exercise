97a65d3f9400f283eef9e901b3cf39e32fe3971d
Lei Wang <wanglei.okay@gmail.com>
2022-06-28 10:19:54 +0300

    bfs: #107

---
+++ data_structure/bfs/107_Binary_Tree_Level_Order_Traversal_II.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
+        queue = [root]
+        res = []
+        
+        while queue:
+            m = len(queue)
+            level = []
+            
+            for i in range(m):
+                node = queue.pop(0)
+                
+                if node.left:
+                    queue.append(node.left)
+                
+                if node.right:
+                    queue.append(node.right)
+                
+                level.append(node.val)
+                
+            res.insert(0, level)
+        
+        return res
