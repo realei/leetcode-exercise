934653dee5c3d7f938e26e4134d0f67f0d344b5d
Lei Wang <wanglei.okay@gmail.com>
2022-08-01 21:00:04 +0300

    sorting: #1331

---
+++ data_structure/sorting/1331_Rank_Transform_of_an_Array.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        ranks = {v: i for i, v in enumerate(sorted(set(arr)), 1)}
+        return [ranks[v] for v in arr]
