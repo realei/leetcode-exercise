b9039a906f7f94b711a496c8c2b6b4e9901fc245
Lei Wang <wanglei.okay@gmail.com>
2022-08-16 22:55:15 +0300

    heap: #703 new

--- data_structure/heap/703_Kth_Largest_Element_in_a_Stream.py
+++ data_structure/heap/703_Kth_Largest_Element_in_a_Stream.py
@@ -1,3 +1,7 @@
+from typing import List
+import heapq
+
+
 class KthLargest:
     """
     TBD
@@ -13,15 +17,30 @@ class KthLargest:
         TBD
         """
         heapq.heappush(self.min_heap, val)
-        
+
         while len(self.min_heap) > self.k:
             heapq.heappop(self.min_heap)
-        
-        return self.min_heap[0]
-        
 
+        return self.min_heap[0]
 
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
+
+class NewKthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.minHeap = nums
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        if len(self.minHeap) < self.k:
+            heapq.heappush(self.minHeap, val)
+        elif val > self.minHeap[0]:
+            heapq.heapreplace(self.minHeap, val)
+
+        return self.minHeap[0]
